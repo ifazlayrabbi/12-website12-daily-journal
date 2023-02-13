7c968f5bb6d3d45a035b12f42e6c33e50b2c39d8
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-02-13 15:43:31 +0600

    ejs layouts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
+require('dotenv').config()
 //lodash
 
 
@@ -17,14 +18,14 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
 
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', (req, res) => res.render('home', {
+    home_starting_content : homeStartingContent
+}))
 
 
 
 
 
 
-
-
-
-app.listen(3000, () =>  console.log("Server started on port 3000"))
+const port = process.env.PORT || 3000
+app.listen(port, () =>  console.log("Server started on port "+port))
