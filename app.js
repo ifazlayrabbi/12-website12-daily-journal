5b69db9ff05858c9f0df86ed9c6582b0b7b7bfec
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-02-18 19:22:06 +0600

    app.get('/posts/'+postBody, (req, res) => {

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
 const compose_value_array = []
-
+let postBody
 
 
 
@@ -42,11 +42,30 @@ app.get('/', (req, res) => {
     })
 })
 
+app.get('/posts/'+postBody, (req, res) => {
+    res.render('home', {
+        home_starting_content: homeStartingContent,
+        compose_array: compose_value_array
+    })
+    console.log(postBody)
+})
 
 
 
 
 
+
+
+
+// app.post('/compose', (req, res) => {
+//     const compose_value_obj = { 
+//         titleValue: req.body.title_value,
+//         postValue: req.body.post_value
+//     }
+
+//     compose_value_array.push(compose_value_obj)
+//     res.redirect('/')
+// })
 
 
 app.post('/compose', (req, res) => {
@@ -56,7 +75,13 @@ app.post('/compose', (req, res) => {
     }
 
     compose_value_array.push(compose_value_obj)
-    res.redirect('/')
+    // res.redirect('/')
+
+    compose_value_array.forEach((element) => {
+        postBody = element.postValue
+        console.log(postBody)
+    })
+    res.redirect('/posts/'+postBody)
 })
 
 
