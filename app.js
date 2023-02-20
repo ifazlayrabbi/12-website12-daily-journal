e36bfb1b9458f60883653e896e1cbdeb083fb4e1
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-02-20 16:20:40 +0600

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 require('dotenv').config()
+const _ = require('lodash')
 
 
 
@@ -16,14 +17,14 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero."
 const compose_value_array = []
-let postBody
+// let postBody
 
 
 
 
 
-const location = ['/about', '/contact', '/compose']
-const file = ['about', 'contact', 'compose']
+const location = ['/', '/about', '/contact', '/compose']
+const file = ['home', 'about', 'contact', 'compose']
 
 for(let i=0; i<location.length; i++){
     app.get(location[i], (req, res) => {
@@ -31,16 +32,10 @@ for(let i=0; i<location.length; i++){
             home_starting_content : homeStartingContent,
             about_content: aboutContent,
             contact_content: contactContent,
+            compose_array: compose_value_array
         })
     })
 }
-
-app.get('/', (req, res) => {
-    res.render('home', {
-        home_starting_content: homeStartingContent,
-        compose_array: compose_value_array
-    })
-})
 
 // app.get('/posts/'+postBody, (req, res) => {
 //     res.render('home', {
@@ -50,28 +45,25 @@ app.get('/', (req, res) => {
 //     console.log(postBody)
 // })
 
-
 app.get('/posts/:topicName', (req, res) => {
     // console.log(req.params.topicName)
     compose_value_array.forEach((element) => {
-        if(element.titleValue == req.params.topicName)
+        if(_.kebabCase(element.titleValue) == _.kebabCase(req.params.topicName)){
             console.log('Successfully Matched!!')
-    })
+            res.render('post', {
+                title:element.titleValue,
+                body: element.postValue
+            })
+        } else {
+            console.log('Not a Match!!')
 
-    res.render('home', {
-        home_starting_content: homeStartingContent,
-        compose_array: compose_value_array
+            res.render('home', {
+                home_starting_content: homeStartingContent,
+                compose_array: compose_value_array
+            })
+        }
     })
 })
-
-
-
-
-
-
-
-
-
 
 
 
@@ -114,7 +106,8 @@ app.post('/compose', (req, res) => {
     }
 
     compose_value_array.push(compose_value_obj)
-    res.redirect('/')
+    // res.redirect('/')
+    res.redirect('/posts/:topicName')
 })
 
 
