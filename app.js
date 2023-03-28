7731bf994606f46b8c9e482d67f47a7943ae61f6
Fazlay Rabbi <83054901+ifazlayrabbi@users.noreply.github.com>
2023-03-28 00:14:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true})) 
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
